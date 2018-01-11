10699a03bf136a98efaf4df3c926a8e237eb08e8
284km <k.furuhashi10@gmail.com>
2018-01-11 14:26:10 +0900

    Rails 3.2.22.5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsUpgradeNoYatu::Application.config.secret_token = '800225b543918bcaf5ba8982d6dc3edfb1a8461e8cc2fed9f1ad548af7f7a0aa16b1dd86337f8965576b13176f3c29079e1aa802ca8848228443bc8edaf6896e'
